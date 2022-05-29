286e0967d0395b1973097bc6f7bbcbfbd0d176d4
Burak Kuyucu <kuyucuburakg@gmail.com>
2022-05-29 18:45:43 +0300

    first commit

---
+++ Assets/Scripts/BlackMirrorGame.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackMirrorGame : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] State startingState;
+
+    State currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = startingState;
+        storyText.text = currentState.getStoryText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var nextStates = currentState.getNextStates();
+
+        if (nextStates.Length == 0 && Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+            return;
+        }
+
+        manageStates(nextStates);
+    }
+
+    private void manageStates(State[] nextStates)
+    {
+
+        if (nextStates.Length == 0) {
+            storyText.text = currentState.getStoryText() + "\n\nGame is over! Press Q to exit!";
+            return;
+        }
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                currentState = nextStates[index];
+            }
+        }
+
+        storyText.text = currentState.getStoryText();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string getStoryText()
+    {
+        return storyText;
+    }
+
+    public State[] getNextStates()
+    {
+        return nextStates;
+    }
+}
